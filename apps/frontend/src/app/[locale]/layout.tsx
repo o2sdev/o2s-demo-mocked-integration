6180625dc68272b42a15b2cb4fb1d79f10c1656f
Marcin Krasowski <marcin.krasowski@hycom.pl>
2025-03-12 14:34:05 +0100

    fox(frontend): switched vercel analytics and insights to the `next` variant

--- apps/frontend/src/app/[locale]/layout.tsx
+++ apps/frontend/src/app/[locale]/layout.tsx
@@ -1,5 +1,5 @@
-import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from '@vercel/speed-insights/react';
+import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import { SessionProvider } from 'next-auth/react';
 import { setRequestLocale } from 'next-intl/server';
 import { Inter } from 'next/font/google';
